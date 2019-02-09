664a25ff8ed445eff42e33561b6c2b5fff1af3f0
'zcc' <'540490410@qq.com'>
2019-02-10 00:04:33 +0800

    dev project

---
+++ src/main/java/cn/mldn/vo/Type.java
@@ -0,0 +1,22 @@
+package cn.mldn.vo;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class Type implements Serializable {
+	private int tid;
+	private String title;
+	public int getTid() {
+		return tid;
+	}
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+}
